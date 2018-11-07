f790d8ebdeb34821a720b3a6eeb0c5acf0be983a
glebacque63 <guillaume.lebacque.tic@gmail.com>
2018-11-07 09:47:27 +0100

    Update src/tp01/TP01.java

--- src/tp01/TP01.java
+++ src/tp01/TP01.java
@@ -4,6 +4,7 @@ public class TP01 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, World !");
+		System.out.println("Hello, World !");
 		int a = multiplier(2, 3);
 		System.out.println(a);
 		
